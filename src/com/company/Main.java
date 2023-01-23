71b69079e70ef0ffb74c4679c61da31b43cf6481
micserv010821 <dotnet290320@gmail.com>
2023-01-23 18:44:51 +0200

    upload all

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,7 +2,26 @@ package com.company;
 
 public class Main {
 
+    // --- JS
+    // (x, y) => {}
+    // function add(a,b) {
+    //  return a + b
+    // }
+    //---------------------------
+
+    public static int add(int a, int b) {
+        return a + b;
+    }
+
     public static void main(String[] args) {
-	// write your code here
+        // write your code here
+        System.out.println("Hello World!");
+
+        //main(new String[] {"a", "b"});
+
+        // --- JS
+        //main(["a", "b"]);
+
+        System.out.println(add(3, 4));
     }
 }
